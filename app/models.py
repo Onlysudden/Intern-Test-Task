7af7e68f78b08e5788112119e1108fda715f87d6
Onlysudden <trofimof09@mail.ru>
2022-06-03 22:24:14 +0300

    Add Database, trying import csv file to Database

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from cgitb import text
+from app import db
+from datetime import datetime
+
+class Docs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rubrics = db.Column(db.Text())
+    text = db.Column(db.Text())
+    created_date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Docs {}>'.format(self.rubrics)
