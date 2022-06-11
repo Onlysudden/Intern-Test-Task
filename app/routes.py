386a903668076626d59f0bf0ee19a81d3f715007
Onlysudden <trofimof09@mail.ru>
2022-06-11 18:28:05 +0300

    Correct return for search and delete

--- app/routes.py
+++ app/routes.py
@@ -3,24 +3,22 @@ from flask import render_template, request
 from app.models import Docs
 from app.search import query_index, query_index_by_id
 
-@app.route('/search/', methods=["GET"])
+@app.get('/search/')
 def get_posts():
     text = request.args["text"]
     if text is None:
-        pass
+        return "Text is None"
     ids = query_index('docs', text)
     result = Docs.query.filter(Docs.id.in_(ids)).order_by(Docs.created_date.desc()).all()
-    #return render_template('search.html', result=result)
-    #return {'result': result}
     return {"result": [post.as_dict() for post in result]}
 
-@app.route('/delete/')
+@app.delete('/delete/')
 def delete_post():
     id = request.args["id"]
     if query_index_by_id('docs', id) is not None:
         elastic = es.delete(index='docs', id=id)
         sqlite = Docs.query.filter(Docs.id==id).delete()
         db.session.commit()
-        return {'sqlite': sqlite}
+        return {'result':{'sqlite':str(sqlite),'elastic':str(elastic)}}
     return render_template('error.html')
 
